852dc83fa4998fb5140fa835dadf2259c954a739
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2026-02-27 00:19:56 -0600

    Update UpDownCounter_TB.sv

--- Testbenches/UpDownCounter_TB.sv
+++ Testbenches/UpDownCounter_TB.sv
@@ -2,31 +2,60 @@
 UpDownCounter Testbench
 */
 
-module UpDownCounter_TB_top;
-
-  reg clk, rst, enable, direction, flag;
+module tb;
+  // set this param in the tb to drive the DUT and the bit widths
+  localparam WIDTH = 8;
+  
+  reg clk, rst, enable, direction;
   reg [WIDTH-1:0] load;
+  wire flag;
 
-  // initialize inputs
-  initial clk = 0;
-  initial rst = 0;
-  initial enable = 0;
-  initial direction = 0;
-  initial flag = 0;
-
-  // clock signal
-  always #10 clk = ~clk;
-
-  UpDownCounter DUT #(
-    .WIDTH(4)
-  )
-  (
+  // initialize DUT and connect IO
+  UpDownCounter #(.WIDTH(WIDTH)) DUT (
     .clk(clk),
     .rst(rst),
     .enable(enable),
     .load(load),
     .direction(direction),
     .flag(flag)
-  )
+  );
+
+  // initialize inputs
+  initial clk = 0;
+  initial rst = 0;
+  initial enable = 0;
+  initial direction = 0;
+
+  // creating the clock signal
+  initial begin
+    // dump waves
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb);
+    
+    // oscillating the clock
+    forever begin
+      #1;
+      clk = ~clk;
+    end
+  end
+
+  // hold reset then release
+  initial begin
+  	rst = 1;
+    #3;
+    rst = 0;
+  end
+
+  // load and count up
+  initial begin
+    #3;
+      
+  end
+  
+  // specify a duration 
+  initial begin
+    #100; // wait for 50 time units
+    $finish;
+  end
   
 endmodule
