7082b7e1934f68986830501a56b8734b0503155c
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2026-03-02 23:43:10 -0600

    Update UpDownCounter_TB.sv

--- Testbenches/UpDownCounter_TB.sv
+++ Testbenches/UpDownCounter_TB.sv
@@ -2,9 +2,12 @@
 UpDownCounter Testbench
 */
 
+// timescale <time_unit> / <time_precision>
+`timescale 1ns/1ns
+
 module tb;
   // set this param in the tb to drive the DUT and the bit widths
-  localparam WIDTH = 3;
+  localparam WIDTH = 4;
   
   reg clk, rst, enable, direction;
   reg [WIDTH-1:0] load;
@@ -20,6 +23,49 @@ module tb;
     .flag(flag)
   );
 
+  // ----Defining the tasks----
+  // Creating a DUT reset for up counting
+  task up_reset(input integer num_clocks);
+    begin
+      rst = 1;
+      enable = 0;
+      direction = 1;
+      repeat (num_clocks) @ (posedge clk);
+      rst = 0;
+    end
+
+  // Creating a DUT reset for down counting
+  task down_reset(input integer num_clocks);
+    begin
+      rst = 1;
+      enable = 0;
+      direction = 0;
+      repeat (num_clocks) @ (posedge clk);
+      rst = 0;
+    end 
+    
+  // Creating an up count task
+  task up_count(input [WIDTH-1:0] load, 
+                input integer num_clocks);
+    begin
+      enable = 1;
+      load = load;
+      direction = 1;
+      repeat (num_clocks) @ (posedge clk);
+      enable = 0;
+    end
+  
+  // Creating a down count task
+  task down_count(input [WIDTH-1:0] load, 
+                  input integer num_clocks);
+    begin
+      enable = 1;
+      load = load;
+      direction = 0;
+      repeat (num_clocks) @ (posedge clk);
+      enable = 0;
+    end
+    
   // creating the clock signal
   initial begin
     // dump waves
@@ -33,20 +79,20 @@ module tb;
     end
   end
 
-  // stimulus
+  // driving stimulus
   initial begin
     // initialize inputs
     clk       = 0;
     rst       = 0;
     enable    = 0;
     direction = 0;
+    repeat (3) @ (posedge clk);
 
-    // load and count up
-    rst = 1;
-    direction = 1;
-    enable = 1;
-    #5;
-    rst = 0;
+    up_reset(2);
+    up_count(4'd5, 20);
+
+    down_reset(3);
+    down_count(4'd6, 30);
 
     // specify a duration 
     #100; // wait for 100 time units
