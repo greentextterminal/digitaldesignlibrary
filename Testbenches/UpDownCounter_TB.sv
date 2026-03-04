eae9fcd046690313c19834c05ef1b0ec998e8baa
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2026-03-04 03:41:05 -0600

    Update UpDownCounter_TB.sv

--- Testbenches/UpDownCounter_TB.sv
+++ Testbenches/UpDownCounter_TB.sv
@@ -36,35 +36,38 @@ module tb;
   endtask
   
   // Creating a DUT reset for up counting
-  task up_reset(input integer num_clocks);
+  task up_reset(input [WIDTH-1:0] test_load,
+                input integer num_clocks);
     begin
       rst       = 1;
       enable    = 0;
       direction = 1;
+      load      = test_load;
       repeat (num_clocks) @ (posedge clk);
       rst       = 0;
     end
   endtask
 
   // Creating a DUT reset for down counting
-  task down_reset(input integer num_clocks);
+  task down_reset(input [WIDTH-1:0] test_load,
+                  input integer num_clocks);
     begin
       rst       = 1;
       enable    = 0;
       direction = 0;
+      load      = test_load;
       repeat (num_clocks) @ (posedge clk);
       rst       = 0;
     end 
   endtask
     
-  // Creating an up count task
-  task up_count(input [WIDTH-1:0] test_load, 
+  // Creating an up count
+  task up_count(input [WIDTH-1:0] test_load,
                 input integer num_clocks);
     begin
       @(posedge clk);  // Wait for the edge
       #0.1;            // Nudge slightly past the edge
       enable    = 1;
-      load      = test_load;
       direction = 1;
       repeat (num_clocks) @ (posedge clk);
       #0.1;
@@ -72,14 +75,13 @@ module tb;
     end
   endtask
   
-  // Creating a down count task
-  task down_count(input [WIDTH-1:0] test_load, 
+  // Creating a down count
+  task down_count(input [WIDTH-1:0] test_load,
                   input integer num_clocks);
     begin
       @(posedge clk);  // Wait for the edge
       #0.1;            // Nudge slightly past the edge
       enable    = 1;
-      load      = test_load;
       direction = 0;
       repeat (num_clocks) @ (posedge clk);
       #0.1;
@@ -106,23 +108,24 @@ module tb;
     clk       = 0;
     rst       = 0;
     enable    = 0;
+    load      = 0;
     direction = 0;
     repeat (3) @ (posedge clk);
     // reset with direction set to up and then count up
-    up_reset(2);
-    up_count(4'd5, 20);
+    up_reset(4, 12);
+    up_count(4, 12);
     // clear out the signals to cleanly see the next test
     signal_clear(4);
     // reset with direction set to down and then count down
-    down_reset(3);
-    down_count(4'd6, 30);
+    down_reset(3, 12);
+    down_count(3, 12);
     // clear out the signals to cleanly see the next test
     signal_clear(4);
     // count up and then down
-    up_reset(1);
-    up_count(4'd4, 4);
-    down_reset(1);
-    down_count(4'd4, 4);
+    up_reset(5, 2);
+    up_count(5, 2);
+    down_reset(5, 2);
+    down_count(5, 2);
 
     // specify a duration 
     #100; // wait for 100 time units
